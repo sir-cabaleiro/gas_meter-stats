246f97bc6e7e54a6a5ca6a908b4da6b9f623eaa0
adrià pérez cabaleiro <perezcabaleiroadria@gmail.com>
2022-11-30 19:01:24 +0100

    Update gas_meter-stats.py

--- gas_meter-stats.py
+++ gas_meter-stats.py
@@ -1,20 +1,55 @@
+#from picamera import PiCamera
+import pytesseract as tess
+from PIL import Image
 import paramiko
 import datetime
 import json
 
-now = datetime.datetime.now()
-mes = str(now.month)
-dia = int(now.day)
-hora = int(now.hour)
-minuto = int(now.minute)
-ahora = {"dia": dia, "hora": hora, "minuto": minuto}
-HOST = ""
-USER = "root"
-PORT = ""
-local = 'C:/Users/adri/Desktop/gas/' + mes + '.json'
-remote = ""
+
+#camera = PiCamera()
+
+def leer_contador() :
+    global contador
+
+    tess.pytesseract.tesseract_cmd = r'<<ruta tesseract>>'
+    my_image = Image.open(r'<<ruta imagen>>')
+    
+    contador = tess.image_to_string(my_image, lang='eng', config='--psm 10 --oem 3 -c tessedit_char_whitelist=0123456789')
+    contador = contador[:-1]
+    print("Lectura del contador: " + contador)
+
+def comprobacion() :
+    global contador
+
+    print(len(contador))
+
+    if len(contador) == 6 :
+        print("Lectura correcta")
+
+    else :
+        print("Lectura no tomada")
+        exit()
+
+def resultado() :
+    global ahora
+    global mes
+
+    now = datetime.datetime.now()
+    mes = str(now.month)
+    dia = int(now.day)
+    hora = int(now.hour)
+    minuto = int(now.minute)
+    ahora = {"dia": dia, "hora": hora, "minuto": minuto, "contador": contador}
+    print(ahora)
 
 def archivo_json():
+    global mes
+    global local
+    global remote
+
+    local = '<<ruta local>>/' + mes + '.json'
+    remote = "<<ruta remota>>/" + mes + ".json"
+
     with open(local) as archivo:
         data = json.load(archivo)
         temp = data["dias"]
@@ -25,10 +60,14 @@ def archivo_json():
         json.dump(data, archivo)
 
 def conexion():
+    HOST = "ip host"
+    USER = "user"
+    PORT = "port"
+
     client = paramiko.SSHClient()
     client.set_missing_host_key_policy( paramiko.AutoAddPolicy() )
     try:
-        client.connect(HOST, port=PORT, username=USER, password='')
+        client.connect(HOST, port=PORT, username=USER, password='pass')
 
         sftp = client.open_sftp()
 
@@ -44,7 +83,16 @@ def conexion():
         print('no se conectó :(')
 
 
+
 if __name__ == '__main__':
+    try:
+        leer_contador()
+        comprobacion()
+    except:
+        print("Lectura no tomada")
+        exit()
+    
+    resultado()
     archivo_json()
     conexion()
 
